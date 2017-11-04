981882fd30837bb7f634f24077b5e99cb4c646bb
Brian Bockelman <bbockelm@cse.unl.edu>
2017-11-03 21:03:49 -0500

    Fix imports.

--- src/x509_scitokens_issuer/utils.py
+++ src/x509_scitokens_issuer/utils.py
@@ -5,8 +5,16 @@ Various helper utility functions for the x509_scitokens_issuer package
 import cryptography.hazmat.primitives.asymmetric.rsa as rsa
 import cryptography.hazmat.primitives.asymmetric.ec as ec
 from cryptography.hazmat.backends import default_backend
-import scitokens.scitokens.utils as scitokens_utils.long_from_bytes
 import scitokens
+
+def long_from_bytes(data):
+    """
+    Return an integer from base64-encoded string.
+
+    :param data: UTF-8 string containing base64-encoded data.
+    :returns: Corresponding decoded integer.
+    """
+    return cryptography.utils.int_from_bytes(decode_base64(data.encode("ascii")), 'big')
 
 def load_jwks(jwks_obj):
     """
@@ -15,9 +23,9 @@ def load_jwks(jwks_obj):
     """
 
     if raw_key['kty'] == "RSA":
-        n = scitokens_utils.long_from_bytes(jwks_obj['n'])
-        e = scitokens_utils.long_from_bytes(jwks_obj['e'])
-        d = scitokens_utils.long_from_bytes(jwks_obj['d'])
+        n = long_from_bytes(jwks_obj['n'])
+        e = long_from_bytes(jwks_obj['e'])
+        d = long_from_bytes(jwks_obj['d'])
         public_key_numbers = rsa.RSAPublicNumbers(
             e = e,
             n = n,
@@ -27,20 +35,20 @@ def load_jwks(jwks_obj):
         if ('p' not in jwks_obj) or ('q' not in jwks_obj):
             p, q = rsa.rsa_recover_prime_factors(n, e, d)
         else:
-            p = scitokens_utils.long_from_bytes(jwks_obj['p'])
-            q = scitokens_utils.long_from_bytes(jwks_obj['q'])
+            p = long_from_bytes(jwks_obj['p'])
+            q = long_from_bytes(jwks_obj['q'])
         if 'qi' not in jwks_obj:
             qi = rsa.rsa_crt_iqmp(p, q)
         else:
-            qi = scitokens_utils.long_from_bytes(jwks_obj['qi'])
+            qi = long_from_bytes(jwks_obj['qi'])
         if 'dp' not in jwks_obj:
             dmp1 = rsa.rsa_crt_dmp1(d, p)
         else:
-            dmp1 = scitokens_utils.long_from_bytes(jwks_obj['dp'])
+            dmp1 = long_from_bytes(jwks_obj['dp'])
         if 'dq' not in jwks_obj:
             dmq1 = rsa.rsa_crt_dmq1(d, q)
         else:
-            dmq1 = scitokens_utils.long_from_bytes(jwks_obj['dq'])
+            dmq1 = long_from_bytes(jwks_obj['dq'])
         private_key_numbers = rsa.RSAPrivateNumbers(
             p = p,
             q = q,
@@ -53,12 +61,12 @@ def load_jwks(jwks_obj):
         return private_key_numbers.private_key()
     elif raw_key['kty'] == 'EC':
         public_key_numbers = ec.EllipticCurvePublicNumbers(
-            scitokens_utils.long_from_bytes(jwks_obj['x']),
-            scitokens_utils.long_from_bytes(jwks_obj['y']),
+            long_from_bytes(jwks_obj['x']),
+            long_from_bytes(jwks_obj['y']),
             ec.SECP256R1
         )
         private_key_numbers = ec.EllipticCurvePrivateNumbers(
-            scitokens_utils.long_from_bytes(jwks_obj['d']),
+            long_from_bytes(jwks_obj['d']),
             public_key_numbers
         )
         return private_key_numbers.private_key()
